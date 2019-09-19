7e895d876546a2c3127540e0847697380e329792
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-19 14:40:44 -0700

    Update Lives.cs

--- Assets/Scriptsa/Lives.cs
+++ Assets/Scriptsa/Lives.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lives : Monobehaviour {
-
-
+  [SerializeField] int lives = 5;
+  [SerializeField] int damage = 1;
+  Text livesText;
+   
+   void Start() 
+   {
+     livesText = GetComponent<Text>();
+     UpdateDisplay();
+   }
+   
+   private void UpdateDisplay()
+     {
+       livesText.text = lives.ToString();
+     }
+   
+   public voidtakeLife()
+   {
+     lives -= damage;
+     UpdateDisplay();
+   }
 }
