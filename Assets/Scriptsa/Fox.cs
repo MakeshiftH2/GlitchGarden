31aa8638b248f128345c2547229f07aea3deacee
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-19 14:13:31 -0700

    Update Fox.cs

--- Assets/Scriptsa/Fox.cs
+++ Assets/Scriptsa/Fox.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fox : MonoBehaviour {
+  
+  private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        if(otherObject.GetComponent<Gravestone>())
+        {
+          GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+        else if (otherObject.GetComponent<Defender>()
+        {
+          GetComponent<Attack>().Attacked(otherObject);
+        }
+    }
 
 
 
