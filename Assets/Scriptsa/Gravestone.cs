361ca28f04d4d78abfdef105a119f2d75a09f872
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-19 14:13:43 -0700

    Update Gravestone.cs

--- Assets/Scriptsa/Gravestone.cs
+++ Assets/Scriptsa/Gravestone.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravestone : MonoBehaviour {
-
-
+  
+  private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+    Attacker attacker = otherCollider.GetComponent<Attack>();
+    if(attacker)
+    {
+      // do a backflip
+    } 
+  
 }
