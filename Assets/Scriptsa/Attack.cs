bb9cf150b2faa2899680481dbe643a5874ed25dc
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-19 12:35:16 -0700

    Update Attack.cs

--- Assets/Scriptsa/Attack.cs
+++ Assets/Scriptsa/Attack.cs
@@ -11,7 +11,15 @@ public class Attack : MonoBehaviour {
   void Update ()
   {
     transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    UpdateAnimationState();
   }
+  
+  private void UpdateAnimationState()
+  {
+    if(!currentTarget)
+    {
+      GetComponent<Animator>().SetBool("isAttacking", false);
+    }
   
   public void SetMovementSpeed(float speed)
   {
@@ -22,6 +30,16 @@ public class Attack : MonoBehaviour {
   {
     GetComponent<Animator>().SetBool("isAttacking", true);
     currentTarget = target;
+  }
+  
+  public void StrikeCurrentTarget(float damage)
+  {
+    if (!currentTarget) { return; }
+    Health health = currentTarget.GetComponent<Health>();
+    if (health)
+    {
+      health.DealDamage(damage):
+    }
   }
   
 }
