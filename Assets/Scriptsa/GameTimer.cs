b2bc1804e5eccb3335cc4d99fc33e53e94bdd274
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-24 14:57:47 -0700

    Update GameTimer.cs

--- Assets/Scriptsa/GameTimer.cs
+++ Assets/Scriptsa/GameTimer.cs
@@ -7,14 +7,17 @@ public class GameTimer : MonoBehaviour {
   
   [Tooltip("Our Level Timer in seconds")]
   [SerializeField] float levelTime = 10;
+  bool triggerLevelFinished = false;
   
   void Update() {
+    if (triggerLevelFinished { return;}
     GetComponent<Slider>().value = Time.timeSinceLevelLoad/levelTime;
     
     bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
     if (timerFinished)
     {
-      Debug.Log("bitch");
+      FindObjectOfType<LevelController>().LevelTimerFinished();
+      triggerLevelFinished = true;
     }
   }
 
