d9a46aaaf87028ceaa4f0049ae5885690b9e5a72
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-24 14:57:44 -0700

    Update AttackerSpawner.cs

--- Assets/Scriptsa/AttackerSpawner.cs
+++ Assets/Scriptsa/AttackerSpawner.cs
@@ -16,6 +16,10 @@ public class AttackerSpawner : MonoBehaviour {
         SpawnAttacker();
       }
     }
+  
+  pubic void StopSpawning() {
+    spawn = false;
+  }
     
     private void SpawnAttacker()
     {
