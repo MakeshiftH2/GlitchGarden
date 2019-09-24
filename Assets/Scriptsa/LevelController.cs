d89eaedbe8dc27bb1ee177d2896eafb83601bc9e
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-24 14:57:41 -0700

    Update LevelController.cs

--- Assets/Scriptsa/LevelController.cs
+++ Assets/Scriptsa/LevelController.cs
@@ -3,5 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour {
+  
+  int numberOfAttackers = 0;
+  bool levelTimerFinished = false;
+  
+  public void AttackerSpawned() {
+    numberOfAttackers++;
+  }
+  
+  public void AttackerKilled() {
+    numberOfAttackers--;
+    if(numberOfAttackers <= 0 && levelTimerFinished){
+      Debug.Log("End Level Now!");
+    }
+  }
+  
+  public void LevelTimerFinished(){
+    levelTimerFinished = true;
+    StopSpawners();
+  }
+  
+  private void StopSpawners() {
+    AttackerSpawners[] spawnerArray = FindObjectsOfType<AttackerSpawner>();
+    foreach (AttackerSpawner spawner in spawnerArray)
+    {
+      spawner.StopSpawning();
+    }
+  }
 
 }
