3f4fcd8a0fd0798c5d1c075ff9213494d048cac1
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-09-25 12:15:32 -0700

    Update LevelController.cs

--- Assets/Scriptsa/LevelController.cs
+++ Assets/Scriptsa/LevelController.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour {
   
+  [SerializeField] GameObject winLabel;
   int numberOfAttackers = 0;
   bool levelTimerFinished = false;
+  [SerializeField] float waitToLoad = 4f
+  
+  private void Start(){
+    winLabel.SetActive(false);
+  }
   
   public void AttackerSpawned() {
     numberOfAttackers++;
@@ -14,8 +20,14 @@ public class LevelController : MonoBehaviour {
   public void AttackerKilled() {
     numberOfAttackers--;
     if(numberOfAttackers <= 0 && levelTimerFinished){
-      Debug.Log("End Level Now!");
+      StartCoroutine(HandleWinCondition());
     }
+  }
+  
+  IEnumerator HandleWinCondition(){
+    winLabel.SetActive("true");
+    yield return new WaitForSeconds(waitToLoad);
+    FindObjectOfType<LevelLoader>().LoadNextScene();
   }
   
   public void LevelTimerFinished(){
