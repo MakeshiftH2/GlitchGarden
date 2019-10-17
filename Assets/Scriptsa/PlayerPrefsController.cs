8cf62af192410414f923f8b6da550cce58034792
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-10-17 12:50:19 -0700

    Update PlayerPrefsController.cs

--- Assets/Scriptsa/PlayerPrefsController.cs
+++ Assets/Scriptsa/PlayerPrefsController.cs
@@ -3,5 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPrefsController : MonoBehaviour {
+  const string MASTER_VOLUME_KEY = "Master Volume";
+  const string DIFFICULTY_KEY = "difficulty";
+  
+  const float MIN_VOLUME = 0f;
+  const float MAX_VOLUME = 1f;
+    
+  public static void SetMasterVolume(float volume)
+  {
+    if (volume >= MIN_VOLUME && volume <= MAX_VOLUME)
+    {
+    Debug.Log("Master volume set to " + volume);
+    PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+    }
+    else
+    {
+      Debug.LogError("Master Volume is out of range!");
+    }
+  }
+  
+  public static float GetMasterVolume()
+  {
+    return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
 
 }
