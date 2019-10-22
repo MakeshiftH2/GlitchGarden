528d3eae5b311e2308a09f18550d73d7f853165b
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-10-22 11:55:59 -0700

    Update OptionsController.cs

--- Assets/Scriptsa/OptionsController.cs
+++ Assets/Scriptsa/OptionsController.cs
@@ -3,8 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OptionsController : MonoBehaviour {
-
-
-
-
+  
+  [SerializeField] Slider volumeSlider;
+  [SerializeField] float defaultVolume = 0.8f;
+  
+  void Start()
+  {
+    volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+  }
+  
+  void Update () {
+    var musicPlayer = FindObjectOfType<MusicPlayer>();
+    if (musicPLayer)
+    {
+      musicPlayer.SetVolume(volumeSlider.value);
+    }
+    
+    else
+    {
+      Debug.LogWarning("NO Music Detected");
+    }
+  }
+  
+  public void SaveAndExit()
+  {
+    PlayerPrefsController.SetMasterVOlume(volumeSlider.value);
+    FindObjectOfType<LevelLoader>().LoadMainMenu();
+  }
+  
+  public void SetDefaults()
+  {
+    volumeSlider.value = defaultVOlume;
+  }
+  
 }
