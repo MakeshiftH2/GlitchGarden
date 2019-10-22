a6a3c7c971d50f8294e2bb9fcbe171f50fc16f51
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-10-22 11:41:21 -0700

    Update MusicPlayer.cs

--- Assets/Scriptsa/MusicPlayer.cs
+++ Assets/Scriptsa/MusicPlayer.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using Unity.Engine;
 
 public class MusicPlayer : MonoBehaviour {
-
-
+  
+  AudioSource audioSource;
+  
+  void Start()
+  {
+    DontDestroyOnLoad(this);
+    audioSource = GetComponent<AudioSource>();
+    audioSource.volume = PlayerPrefsController.GetMasterVolume();
+  }
+  
+  public void SetVolume(float volume)
+  {
+    audioSource.volume = volume;
+  
 }
