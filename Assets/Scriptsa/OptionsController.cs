9b7aa4009319568bb2b05792a7d427a9ae86f832
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-10-22 12:15:59 -0700

    Update OptionsController.cs

--- Assets/Scriptsa/OptionsController.cs
+++ Assets/Scriptsa/OptionsController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class OptionsController : MonoBehaviour {
   
   [SerializeField] Slider volumeSlider;
+  [SerializeField] Slider difficultySlider;
   [SerializeField] float defaultVolume = 0.8f;
+  [SerializeField] float defaultDifficulty = 0;
   
   void Start()
   {
     volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    difficultySlider.value = PlayerPrefsController.GetDifficulty();
   }
   
   void Update () {
@@ -28,12 +31,14 @@ public class OptionsController : MonoBehaviour {
   public void SaveAndExit()
   {
     PlayerPrefsController.SetMasterVOlume(volumeSlider.value);
+    PlayerPrefsController.SetDIfficulty(DifficultySlider.value);
     FindObjectOfType<LevelLoader>().LoadMainMenu();
   }
   
   public void SetDefaults()
   {
     volumeSlider.value = defaultVOlume;
+    difficultySlider.value = defaultDifficulty;
   }
   
 }
