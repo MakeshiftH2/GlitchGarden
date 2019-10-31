e3219540b77d3246515de6cc30c2cf43a6115599
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-10-31 12:29:57 -0700

    Update DefenderButton.cs

--- Assets/Scriptsa/DefenderButton.cs
+++ Assets/Scriptsa/DefenderButton.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour {
   
   [SerializeField] Defender defenderPrefab;
   
   // Create colliders for the sprites!
+  
+  private void Start()
+  {
+    LabelButtonWithCost();
+  }
+  
+  private void LabelButtonWithCost()
+  {
+    Text costText = GetComponentInChildren<Text>();
+    if (!costText)
+    {
+      Debug.Logerror(name + " has  no cost text, add some!");
+    }
+    else
+    {
+      costText.text = defenderPrefab.GetStarCost().ToString();
   
   public void OnMouseDown()
     {
