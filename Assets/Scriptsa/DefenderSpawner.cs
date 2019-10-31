da29703670d7a0ad55a123c4de3820d2bcbc13e4
MakeshiftH2 <39418044+MakeshiftH2@users.noreply.github.com>
2019-10-31 12:05:30 -0700

    Update DefenderSpawner.cs

--- Assets/Scriptsa/DefenderSpawner.cs
+++ Assets/Scriptsa/DefenderSpawner.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class DefenderSpawner : MonoBehaviour {
 
     Defender defender;
+    GameObject defenderParent;
+    const String DEFENDER_PARENT_NAME = "Defenders";
+    
+    private void Start()
+    {
+        CreateDefenderParent();
+    }
+    
+    private void CreateDefenderParent()
+    {
+        defenderParent = GameObject.Find(DEFENDER_PARENT_NAME);
+        if(!defenderParent)
+        {
+            defenderParent = new GameObject(Defender_PARENT_NAME);
+        }
 
     private void OnMouseDown()
     {
@@ -45,7 +60,7 @@ public class DefenderSpawner : MonoBehaviour {
     private void SpawnDefender(Vector2 roundedPos)
     {
         Defender newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as Defender;
-        Debug.Log(roundedPos);
+        newDefender.transform.parent = defenderParent.transform;
     }
 
 }
